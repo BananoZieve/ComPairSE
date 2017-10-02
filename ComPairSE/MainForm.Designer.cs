ad85a3cf17ff3bd93f7d8bbea1ca72b477dbd601
aukstuleviciute <SabinaAukstuleviciute@gmail.com>
2017-10-02 14:01:56 +0300

    Add button "Check" and functionality to clarify words with .

--- ComPairSE/MainForm.Designer.cs
+++ ComPairSE/MainForm.Designer.cs
@@ -32,15 +32,15 @@
             this.tbInput = new System.Windows.Forms.TextBox();
             this.btSubmit = new System.Windows.Forms.Button();
             this.button_SearchItemsByTag = new System.Windows.Forms.Button();
+            this.buttonCheck = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btRnd
             // 
             this.btRnd.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.btRnd.Location = new System.Drawing.Point(48, 310);
-            this.btRnd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btRnd.Location = new System.Drawing.Point(29, 287);
             this.btRnd.Name = "btRnd";
-            this.btRnd.Size = new System.Drawing.Size(128, 27);
+            this.btRnd.Size = new System.Drawing.Size(96, 22);
             this.btRnd.TabIndex = 0;
             this.btRnd.Text = "Random Test";
             this.btRnd.UseVisualStyleBackColor = true;
@@ -52,20 +52,18 @@
             this.tbInput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.tbInput.Location = new System.Drawing.Point(4, 4);
-            this.tbInput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbInput.Location = new System.Drawing.Point(3, 3);
             this.tbInput.Multiline = true;
             this.tbInput.Name = "tbInput";
-            this.tbInput.Size = new System.Drawing.Size(487, 302);
+            this.tbInput.Size = new System.Drawing.Size(448, 280);
             this.tbInput.TabIndex = 1;
             // 
             // btSubmit
             // 
             this.btSubmit.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.btSubmit.Location = new System.Drawing.Point(184, 310);
-            this.btSubmit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btSubmit.Location = new System.Drawing.Point(137, 287);
             this.btSubmit.Name = "btSubmit";
-            this.btSubmit.Size = new System.Drawing.Size(100, 27);
+            this.btSubmit.Size = new System.Drawing.Size(75, 22);
             this.btSubmit.TabIndex = 2;
             this.btSubmit.Text = "Submit";
             this.btSubmit.UseVisualStyleBackColor = true;
@@ -73,25 +71,34 @@
             // 
             // button_SearchItemsByTag
             // 
-            this.button_SearchItemsByTag.Location = new System.Drawing.Point(292, 309);
-            this.button_SearchItemsByTag.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button_SearchItemsByTag.Location = new System.Drawing.Point(222, 287);
             this.button_SearchItemsByTag.Name = "button_SearchItemsByTag";
-            this.button_SearchItemsByTag.Size = new System.Drawing.Size(161, 28);
+            this.button_SearchItemsByTag.Size = new System.Drawing.Size(120, 22);
             this.button_SearchItemsByTag.TabIndex = 3;
             this.button_SearchItemsByTag.Text = "Search items by tag";
             this.button_SearchItemsByTag.UseVisualStyleBackColor = true;
             this.button_SearchItemsByTag.Click += new System.EventHandler(this.button_SearchItemsByTag_Click);
             // 
+            // buttonCheck
+            // 
+            this.buttonCheck.Location = new System.Drawing.Point(351, 287);
+            this.buttonCheck.Name = "buttonCheck";
+            this.buttonCheck.Size = new System.Drawing.Size(75, 22);
+            this.buttonCheck.TabIndex = 4;
+            this.buttonCheck.Text = "Check";
+            this.buttonCheck.UseVisualStyleBackColor = true;
+            this.buttonCheck.Click += new System.EventHandler(this.buttonCheck_Click);
+            // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(508, 341);
+            this.ClientSize = new System.Drawing.Size(463, 311);
+            this.Controls.Add(this.buttonCheck);
             this.Controls.Add(this.button_SearchItemsByTag);
             this.Controls.Add(this.btSubmit);
             this.Controls.Add(this.tbInput);
             this.Controls.Add(this.btRnd);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ComPairSE";
@@ -108,6 +115,7 @@
         private System.Windows.Forms.Button btSubmit;
         private System.Windows.Forms.TextBox tbInput;
         private System.Windows.Forms.Button button_SearchItemsByTag;
+        private System.Windows.Forms.Button buttonCheck;
     }
 }
 
