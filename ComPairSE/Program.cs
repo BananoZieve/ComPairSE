b5e20c0fabd64b836fe9aae614e9da4822ea6755
lukas.akstinas <lukas.akstinas@gmail.com>
2017-10-16 13:39:57 +0300

    Modify Program to accept command line args

--- ComPairSE/Program.cs
+++ ComPairSE/Program.cs
@@ -13,12 +13,24 @@ namespace ComPairSE
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(params string[] args)
         {
-           
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            if (args.Length == 0)
+                Application.Run(new MainForm());
+            else if (args.Length == 1)
+            {
+                switch (args[0])
+                {
+                    case "-v2":
+                        Application.Run(new MainForm2());
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
