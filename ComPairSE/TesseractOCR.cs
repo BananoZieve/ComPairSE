76c02ed166f19dc2670a67cb19f65cdde81c0181
Rūta Jankauskaitė <rutule9@gmail.com>
2017-11-02 18:45:32 +0200

    Add OCR config file and user-words user-patterns

--- ComPairSE/TesseractOCR.cs
+++ ComPairSE/TesseractOCR.cs
@@ -18,7 +18,7 @@ namespace ComPairSE
         public string GetText(Bitmap bmp)
         {
             var ocrtext = string.Empty;
-            using (var engine = new TesseractEngine(@"../../tessdata", "lit", EngineMode.Default))
+            using (var engine = new TesseractEngine(@"../../tessdata", "lit", EngineMode.Default, @"../../config"))
             {
                 using (var img = PixConverter.ToPix(bmp))
                 {
